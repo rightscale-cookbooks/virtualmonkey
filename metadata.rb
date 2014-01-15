3818330f6bfca88333e6133f162f45681c1e27e2
Jon Marinello <jon.marinello@rightscale.com>
2014-01-15 09:58:12 -0800

    Removed “This is required for all next generation collateral. If you are using legacy collateral, just put an 'X' in this input." from the attribute "virtualmonkey/rocketmonkey/servertemplate_mapping_file_name" description.

--- metadata.rb
+++ metadata.rb
@@ -343,7 +343,7 @@ attribute "virtualmonkey/rocketmonkey/repo_branch",
 attribute "virtualmonkey/rocketmonkey/servertemplate_mapping_file_name",
   :display_name => "Rocket Monkey Server Template Mapping File Name",
   :description =>
-      "The server template mapping file name you want the Rocket Monkey to use when generating Virtual Monkey Jenkins jobs. This is required for all next generation collateral. If you are using legacy collateral, just put an 'X' in this input.",
+      "The server template mapping file name you want the Rocket Monkey to use when generating Virtual Monkey Jenkins jobs.",
   :required => "required",
   :recipes => ["virtualmonkey::setup_rocketmonkey"]
 
